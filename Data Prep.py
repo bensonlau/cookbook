65c50b4684355cea0efb20fe9e1b6daffd5de6ae
Benson Lau <Benson.Lau@nike.com>
2023-05-04 23:56:27 -0700

    Renaming from 'loading' to 'prep'
    also added in options for saving and loading data, adding/removing/renaming

--- Data Prep.py
+++ Data Prep.py
@@ -2,25 +2,36 @@ from sklearn import datasets
 #https://scikit-learn.org/0.15/modules/classes.html#module-sklearn.datasets
 
 
-##### Pyspark ######
+##### Pyspark ######aßßssssssssss
 
 #Loading Data
+#...from SQL databases
 query = """[sql query]]"""
 df = spark.sql(query)
 
+#...from csv
 df = spark.read.format('csv').option('header','true').load('[s3://desired_path]')
+
+
+#Saving Data
+#....to s3 location
+df.write.mode("overwrite").option("path", "s3://desired_path").saveAsTable("schema.tableName")
+
+#...to csv
+df.write.mode('overwrite').csv(file_path, header = 'true')
 
 ##Joining Data
 df3 = df1.join(df2, df1.colName ==  df2.colName,"inner")
 
-#Saving Data
-df3.write.mode("overwrite").option("path", "s3://desired_path]").saveAsTable("schema.tableName")
 
 # Adding & Removing
 df.drop() #column name
 
 #Renaming
 df = df.withColumnRenamed("existing","new")
+
+#Casting
+df.withColumn("age",df.age.cast('integer'))
 
 #Merging
 
