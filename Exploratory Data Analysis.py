8f62acd7f1aa666243b21dc3dc67fe2b4a070b87
Benson Lau <Benson.Lau@nike.com>
2023-05-25 22:53:31 -0700

    Adding in pyspark built-in functions, user defined percent_missing and built-in pandas functions
    Adding in pyspark built-in functions, user defined percent_missing function and built-in pandas functions

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -1,6 +1,14 @@
-
 ##### Pyspark #####
 
+#summary statistics of columns present in the data frame
+df.describe()
+
+
+print('Data overview')
+df.printSchema()
+
+print('Columns overview')
+pd.DataFrame(df.dtypes, columns = ['Column Name','Data type'])
 
 #FIlter singular column
 df.filter(df.state == "OH").show(truncate=False)
@@ -20,3 +28,27 @@ print("spark_shape(sparkDf) loaded")
 print("Using count()  to get the number of rows on DataFrame and len(df.columns()) to get the number of columns.")
 
 ##### Pandas #####
+# Printing the percentage of missing values per column
+def percent_missing(pandasDf):
+    '''
+    Prints the percentage of missing values for each column in a dataframe
+    '''
+    # Summing the number of missing values per column and then dividing by the total rows
+    sumMissing = dataframe.isnull().values.sum(axis=0)
+    pctMissing = sumMissing / dataframe.shape[0]
+    
+    if sumMissing.sum() == 0:
+        print('No missing values')
+    else:
+        # Looping through and printing out each columns missing value percentage
+        print('Percent Missing Values:', '\n')
+        for idx, col in enumerate(dataframe.columns):
+            if sumMissing[idx] > 0:
+                print('{0}: {1:.2f}%'.format(col, pctMissing[idx] * 100))
+        
+
+##### Pandas #####
+df.head()
+df["column_name"].mean()
+df["column_name"].median()
+df["column_name"].describe()
