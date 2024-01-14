371edae7bf8e48fb2fd5990e16ff6d958e4b3d01
Benson Lau <bensonjaclyn.lau@gmail.com>
2024-01-14 12:45:26 -0800

    converting between pandas and pyspark data frames, merging data frames
    converting between pandas and pyspark data frames, merging data frames

--- Data Prep.py
+++ Data Prep.py
@@ -1,8 +1,20 @@
 from sklearn import datasets
 #https://scikit-learn.org/0.15/modules/classes.html#module-sklearn.datasets
 
+####################
+##### Pyspark ######
+####################
 
-##### Pyspark ######aßßssssssssss
+#Converting from Pandas
+# SparkSession
+from pyspark.sql import SparkSession
+ 
+# Building the SparkSession and name
+# it :'pandas to spark'
+spark = SparkSession.builder.appName(
+  "pandas to spark").getOrCreate()
+
+sparkDF=spark.createDataFrame(pandasDF) 
 
 #Loading Data
 #...from SQL databases
@@ -24,8 +36,12 @@ df.write.mode('overwrite').csv(file_path, header = 'true')
 df3 = df1.join(df2, df1.colName ==  df2.colName,"inner")
 
 
-# Adding & Removing
-df.drop() #column name
+# Adding & Removing columns
+df.select("column_name")
+df.drop("column_name") 
+
+#Sorting
+df.sort_values(by=['col1','col2'], ascending=False)
 
 #Renaming
 df = df.withColumnRenamed("existing","new")
@@ -34,6 +50,27 @@ df = df.withColumnRenamed("existing","new")
 df.withColumn("age",df.age.cast('integer'))
 
 #Merging
+df3 = df1.join(df2, df1.store_Id == df2.store_Id2,"left")
+df3 = df1.join(df2,["store_id"])
 
-df = df.join(spark_table, df.store_Id == spark_table.store_Id2,"left")
-df = df.join(spark_table,["store_id"])
+
+####################
+##### Pandas ######
+####################
+import pandas as pd
+
+#Converting from Pyspark to Pandas
+pandasDf = sparkDf.toPandas()
+
+#Merging
+df3 = df1.merge(df2, how='inner', on='a')
+
+def confirmation_rate(signups: pd.DataFrame, confirmations: pd.DataFrame) -> pd.DataFrame:
+    return (
+        pd.merge(signups[["user_id"]], confirmations[["action", "user_id"]], on="user_id", how="left")
+        .assign(confirmation_rate=lambda frame: frame.action.apply(lambda action: action == "confirmed"))
+        .groupby("user_id", as_index=False)
+        .confirmation_rate
+        .mean()
+        .round(2)
+    )
