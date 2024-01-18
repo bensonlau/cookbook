9820593853c8c0d315355b5e54bcbf80827e1dbf
Benson Lau <bensonjaclyn.lau@gmail.com>
2024-01-17 19:41:55 -0800

    updating to include type hinting for percent_missing udf
    updating to include type hinting for percent_missing udf

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -1,8 +1,7 @@
 ##### Pyspark #####
 
-#summary statistics of columns present in the data frame
+#summary statistics of columns present in the data frame (df)
 df.describe()
-
 
 print('Data overview')
 df.printSchema()
@@ -28,14 +27,19 @@ print("spark_shape(sparkDf) loaded")
 print("Using count()  to get the number of rows on DataFrame and len(df.columns()) to get the number of columns.")
 
 ##### Pandas #####
+import pandas
+
 # Printing the percentage of missing values per column
-def percent_missing(pandasDf):
+def percent_missing(df:pd.DataFrame)->str:
     '''
-    Prints the percentage of missing values for each column in a dataframe
+    Derives the percentage of missing values for each column in a dataframe
+
+    Args:
+        df
     '''
     # Summing the number of missing values per column and then dividing by the total rows
-    sumMissing = dataframe.isnull().values.sum(axis=0)
-    pctMissing = sumMissing / dataframe.shape[0]
+    sumMissing = df.isnull().values.sum(axis=0)
+    pctMissing = sumMissing / df.shape[0]
     
     if sumMissing.sum() == 0:
         print('No missing values')
@@ -43,10 +47,52 @@ def percent_missing(pandasDf):
         # Looping through and printing out each columns missing value percentage
         print('Percent Missing Values:', '\n')
         for idx, col in enumerate(dataframe.columns):
+        for idx, col in enumerate(df.columns):
             if sumMissing[idx] > 0:
                 print('{0}: {1:.2f}%'.format(col, pctMissing[idx] * 100))
+
+print("Number of Rows: ", rows)
+print("Number of Columns: ", cols)
+
         
 df.head()
 df["column_name"].mean()
 df["column_name"].median()
-df["column_name"].describe()
+df["column_name"].describe()        .reset_index()
+    )
+
+    # Rename columns for clarity
+    monthly_summary.columns = [
+        "month",
+        "country",
+        "trans_count",
+        "approved_count",
+        "trans_total_amount",
+        "approved_total_amount",
+    ]
+
+    return monthly_summary
+
+def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    # fmt: off
+    return (
+        transactions
+        .assign(
+            month=lambda frame: frame.trans_date.apply(lambda date: date.strftime("%Y-%m")),
+            approved=lambda frame: frame[["state", "amount"]].apply(
+                lambda row: row.amount if row.state == "approved" else None,
+                axis=1,
+            ),
+        )
+        .drop(columns=["trans_date"])
+        .groupby(["month", "country"])
+        .agg(
+            trans_count=("state", "count"),
+            approved_count=("approved", "count"),
+            trans_total_amount=("amount", "sum"),
+            approved_total_amount=("approved", "sum"),
+        )
+        .reset_index()
+    )
+    # fmt: on
+
