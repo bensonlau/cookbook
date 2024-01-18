c435f5c3ba0eb43bdbfe411b6a896fb28e579027
Benson Lau <bensonjaclyn.lau@gmail.com>
2024-01-17 20:49:34 -0800

    refactoring percent_missing function
    refactoring percent_missing function to return a data frame

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -40,6 +40,31 @@ def percent_missing(df:pd.DataFrame)->str:
     # Summing the number of missing values per column and then dividing by the total rows
     sumMissing = df.isnull().values.sum(axis=0)
     pctMissing = sumMissing / df.shape[0]
+def percent_missing(df:pd.DataFrame) -> pd.DataFrame:
+  '''
+  Derives the percentage of missing values for each column in a dataframe
+
+  Args:
+      df = pandas dataframe
+
+  Returns
+      pandas dataframe with column names and percentage of missing values in respective columns
+  '''
+  col_names_list=[]
+  pct_missing_list=[]
+
+  # Summing the number of missing values per column and then dividing by the total rows
+  sum_missing = df.isnull().values.sum(axis=0)
+  pct_missing = sum_missing / df.shape[0]
+
+  for idx, col in enumerate(df.columns):
+    col_names_list.append(col)
+    pct_missing_list.append(pct_missing[idx])
+    print ('{0}: {1:.2f}%'.format(col, pctMissing[idx] * 100))
+
+  # Create dataframe
+  df_results = pd.DataFrame({'Columns': col_names_list, "Percent Missing": pct_missing_list})
+  return df_results
     
     if sumMissing.sum() == 0:
         print('No missing values')
