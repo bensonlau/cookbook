eaacf9f2c970a2b45c42dec72bb0cd77ee1a5337
Benson Lau <bensonjaclyn.lau@gmail.com>
2024-02-21 23:12:17 -0800

    adding in common commands for analyzing pandas dataframes.
    adding in common commands for analyzing pandas dataframes.

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -1,7 +1,9 @@
 ##### Pyspark #####
 
 #summary statistics of columns present in the data frame (df)
-df.describe()
+#add .display() in databricks for friendlier visualization
+df.describe() 
+df.summary() #functions like describe() but provides values at 25th, 50th, 75th percentiles
 
 print('Data overview')
 df.printSchema()
@@ -9,7 +11,7 @@ df.printSchema()
 print('Columns overview')
 pd.DataFrame(df.dtypes, columns = ['Column Name','Data type'])
 
-#FIlter singular column
+#Filter singular column
 df.filter(df.state == "OH").show(truncate=False)
 
 # Filter multiple conditi don
@@ -33,11 +35,21 @@ rows = len(df.axes[0])
  
 #...number of columns
 cols = len(df.axes[1])
-
 print("Number of Rows: ", rows)
 print("Number of Columns: ", cols)
         
-df.head()
+# - Below are some transformations you can perform on Pandas DataFrame. Note that statistical functions calculate at each column by default. you don’t have to explicitly specify on what column you wanted to apply the statistical functions. Even count() function returns count of each column (by ignoring null/None values).
+
+df.count() # – Returns the count of each column (the count includes only non-null values).
+df.corr() # – Returns the correlation between columns in a data frame.
+df.head(n) # – Returns first n rows from the top.
+df.max() # – Returns the maximum of each column.
+df.mean() # – Returns the mean of each column.
+df.median() # – Returns the median of each column.
+df.min() # – Returns the minimum value in each column.
+df.std() # – Returns the standard deviation of each column
+df.tail(n) # – Returns last n rows.
+
 df["column_name"].mean()
 df["column_name"].median()
 df["column_name"].describe()
