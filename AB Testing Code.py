a97d36eccce6e2a6cbbb76d4cb4018d8ba69debc
Benson Lau <bensonjaclyn.lau@gmail.com>
2024-02-26 23:39:57 -0800

    a function for evaluating a/b testa

--- AB Testing Code.py
+++ AB Testing Code.py
@@ -1,4 +1,4 @@
-####A/B Testing Code
+####A/B Testing Cookbook
 import numpy as np
 from scipy.stats import stats, shapiro
 
@@ -10,3 +10,71 @@ x = np.array([148, 154, 158, 160, 161, 162, 166, 170, 182, 195, 236])
 
 test_stat, pvalue = shapiro(x)
 print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
+
+# A/B Testing Function - Quick Solution
+def AB_Test(dataframe, group, target):
+    
+    # Packages
+    from scipy.stats import shapiro
+    import scipy.stats as stats
+    
+    # Split A/B
+    groupA = dataframe[dataframe[group] == "A"][target]
+    groupB = dataframe[dataframe[group] == "B"][target]
+    
+    # Assumption: Normality
+    ntA = shapiro(groupA)[1] < 0.05
+    ntB = shapiro(groupB)[1] < 0.05
+    # H0: Distribution is Normal! - False
+    # H1: Distribution is not Normal! - True
+    
+    if (ntA == False) & (ntB == False): # "H0: Normal Distribution"
+        # Parametric Test
+        # Assumption: Homogeneity of variances
+        leveneTest = stats.levene(groupA, groupB)[1] < 0.05
+        # H0: Homogeneity: False
+        # H1: Heterogeneous: True
+        
+        if leveneTest == False:
+            # Homogeneity
+            ttest = stats.ttest_ind(groupA, groupB, equal_var=True)[1]
+            # H0: M1 == M2 - False
+            # H1: M1 != M2 - True
+        else:
+            # Heterogeneous
+            ttest = stats.ttest_ind(groupA, groupB, equal_var=False)[1]
+            # H0: M1 == M2 - False
+            # H1: M1 != M2 - True
+    else:
+        # Non-Parametric Test
+        ttest = stats.mannwhitneyu(groupA, groupB)[1] 
+        # H0: M1 == M2 - False
+        # H1: M1 != M2 - True
+        
+    # Result
+    temp = pd.DataFrame({
+        "AB Hypothesis":[ttest < 0.05], 
+        "p-value":[ttest]
+    })
+    temp["Test Type"] = np.where((ntA == False) & (ntB == False), "Parametric", "Non-Parametric")
+    temp["AB Hypothesis"] = np.where(temp["AB Hypothesis"] == False, "Fail to Reject H0", "Reject H0")
+    temp["Comment"] = np.where(temp["AB Hypothesis"] == "Fail to Reject H0", "A/B groups are similar!", "A/B groups are not similar!")
+    
+    # Columns
+    if (ntA == False) & (ntB == False):
+        temp["Homogeneity"] = np.where(leveneTest == False, "Yes", "No")
+        temp = temp[["Test Type", "Homogeneity","AB Hypothesis", "p-value", "Comment"]]
+    else:
+        temp = temp[["Test Type","AB Hypothesis", "p-value", "Comment"]]
+    
+    # Print Hypothesis
+    print("# A/B Testing Hypothesis")
+    print("H0: A == B")
+    print("H1: A != B", "\n")
+    
+    return temp
+    
+    
+    
+# Apply A/B Testing
+AB_Test(dataframe=ab, group = "version", target = "sum_gamerounds")
